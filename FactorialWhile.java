47901500cb5cdcddd4b40b34f09befdc11432663
Rakshit21 <srakshit373@gmail.com>
2025-08-25 14:35:27 +0530

    Level1

---
+++ FactorialWhile.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class FactorialWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer:
